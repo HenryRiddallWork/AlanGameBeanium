ba18df29aaf1f5582c384f449ed0691c48bff720
Olly Larkin <olly.larkin@softwire.com>
2025-08-09 16:26:45 +0100

    Closest catch

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,16 +1,16 @@
 class_name Player extends RigidBody2D
 
-var hooked: bool = false
-@onready var ray_cast_2d: RayCast2D = $RayCast2D
-@onready var line = $Line2D
-@onready var line_end = hook.get_node("Marker2D")
-@onready var direction_hook: Sprite2D = $DirectionHook
-
 @export var hook: StaticBody2D
 @export var pinjoint : PinJoint2D
 @export var speed = 5
 @export var swing_speed = 3
 @export var hook_range = 500
+@export var raycast_count = 10
+
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var line = $Line2D
+@onready var line_end = hook.get_node("Marker2D")
+@onready var direction_hook: Sprite2D = $DirectionHook
 
 
 func _ready() -> void:
--- scripts/player/states/in_air.gd
+++ scripts/player/states/in_air.gd
@@ -11,16 +11,28 @@ func physics_update(delta: float) -> void:
 		player.direction_hook.global_rotation = joystickDirection
 		
 		if Input.is_action_just_pressed("shoot"):
-			player.ray_cast_2d.target_position = player.to_local(player.global_position + (Vector2.from_angle(joystickDirection - (PI / 2)) * player.hook_range))
-			player.ray_cast_2d.force_raycast_update()
-			if player.ray_cast_2d.is_colliding():
-				#get values from raycast
-				var hook_pos = player.ray_cast_2d.get_collision_point()
-				var collider = player.ray_cast_2d.get_collider()
-				
-				#if the ray collides with a hookable object, move pinjoint and hook to it
-				if collider.is_in_group("Hookable"):
-					finished.emit(HOOKED, {"hook_global_pos": hook_pos})
+			var collision_points = _get_raycast_angles_for_arc(joystickDirection).map(
+				func(angle):
+					player.ray_cast_2d.target_position = player.to_local(player.global_position + (Vector2.from_angle(angle - (PI / 2)) * player.hook_range))
+					player.ray_cast_2d.force_raycast_update()
+					if player.ray_cast_2d.is_colliding():
+						var hook_pos = player.ray_cast_2d.get_collision_point()
+						var collider = player.ray_cast_2d.get_collider()
+						if collider.is_in_group("Hookable"):
+							print("Found collision point:", hook_pos)
+							return hook_pos
+						else:
+							return null
+					else:
+						return null
+			)
+			
+			collision_points = collision_points.filter(func(v): return v != null)
+			collision_points.sort_custom(func(v1, v2): return v1.distance_to(player.global_position) < v2.distance_to(player.global_position))
+			
+			if collision_points.size() > 0:
+				finished.emit(HOOKED, {"hook_global_pos": collision_points[0]})
+				return
 	else:
 		player.direction_hook.visible = false
 
@@ -58,3 +70,16 @@ func _get_joystick_direction() -> float:
 
 func _is_joystick_in_use() -> bool:
 	return Input.is_action_pressed("up") or Input.is_action_pressed("right") or Input.is_action_pressed("down") or Input.is_action_pressed("left")
+
+
+func _get_raycast_angles_for_arc(angle: float) -> Array[float]:
+	# PI / 4 arc around direction
+	var min_angle = angle - (PI / 8)
+	var max_angle = angle + (PI / 8)
+	var step = PI / (4 * player.raycast_count)
+	
+	var out_arr: Array[float] = []
+	for i in range(0, player.raycast_count):
+		out_arr.append(min_angle + (step * i))
+	
+	return out_arr
