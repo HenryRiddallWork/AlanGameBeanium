c1c73af7d16d6ace0c5d49693045934271f5b55a
Freya <freya.connor@softwire.com>
2025-08-11 20:20:46 +0100

    add bsckground

--- scripts/game.gd
+++ scripts/game.gd
@@ -14,7 +14,6 @@ var current_collision_timeout: float = 0
 func _ready() -> void:
 	player_1.player_collision.connect(_on_player_collision)
 	player_2.player_collision.connect(_on_player_collision)
-	_update_player_health_labels()
 
 
 func _process(delta: float) -> void:
@@ -35,9 +34,4 @@ func _on_player_collision(player_speeds: Dictionary) -> void:
 func _damage_players(player_1_damage: int, player_2_damage: int) -> void:
 	Globals.player_data[Globals.PLAYER_1_ID].health = max(0, Globals.player_data[Globals.PLAYER_1_ID].health - player_1_damage)
 	Globals.player_data[Globals.PLAYER_2_ID].health = max(0, Globals.player_data[Globals.PLAYER_2_ID].health - player_2_damage)
-	_update_player_health_labels()
-
-
-func _update_player_health_labels() -> void:
-	player_1_health.text = str(Globals.player_data[Globals.PLAYER_1_ID].health)
-	player_2_health.text = str(Globals.player_data[Globals.PLAYER_2_ID].health)
+	
